6eb5ae783002bf7503b08a625c9cfce3267df93a
mmbmatteo <matteom.bianchini@gmail.com>
2016-05-02 16:31:44 +0200

    Added POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-Parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed the cat',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+app.use(bodyParser.json());
 
 //GET /[root]
 app.get('/', function (req, res) {
@@ -37,6 +29,14 @@ app.get('/todos/:id', function (req, res) {
 		}
 	});
 	res.status(404).send();
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	res.json(body);
 });
 
 app.listen(PORT, function (){
